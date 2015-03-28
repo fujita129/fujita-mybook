7d2359e73b3597d48e137e84410cc8ff790ea8ca
fujita129 <shunfujita1222@gmail.com>
2015-03-28 21:44:01 +0900

    staticfile->static

--- mybook/settings.py
+++ mybook/settings.py
@@ -97,9 +97,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+#STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, STATIC_ROOT),
+    os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
